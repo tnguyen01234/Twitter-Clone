442a6556c44ed5cbac9878339b71091558ec9e90
TommyZenbookPro <nguyentommy735@gmail.com>
2022-12-15 13:18:37 +1100

    Delete and Create post fixed

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,29 +1,82 @@
 import React, { useEffect, useState } from "react";
 import "./css/Feed.css";
+import "./css/TweetBox.css";
 import Post from "./Post";
 import TweetBox from "./TweetBox";
 import { db, auth } from "./firebase.js";
-import { setDoc, doc, onSnapshot, collection, QuerySnapshot, getDoc, getDocs } from "firebase/firestore";
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import {
+  setDoc,
+  doc,
+  onSnapshot,
+  collection,
+  QuerySnapshot,
+  getDoc,
+  getDocs,
+  addDoc,
+} from "firebase/firestore";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { Avatar, Button } from "@mui/material";
+import image from "../assets/IMG_4691.JPG";
+import FlipMove from "react-flip-move";
 
 export default function Feed() {
   const [posts, setPosts] = useState([]);
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
 
- useEffect(() => {
-  const fetchData = async () => {
-    const { docs } = await getDocs(collection(db, 'posts'))
-      setPosts(docs.map((elem) => ({...elem.data(), id: elem.id})))
-      console.log(posts)
+ 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { docs } = await getDocs(collection(db, "posts"));
+      setPosts(docs.map((elem) => ({ ...elem.data(), id: elem.id })));
+    };
+    fetchData()
+  }, [createPost]);
+
+  function createPost(e) {
+    e.preventDefault();
+    const post = {
+      displayName: "Tommy Nguyen",
+      username: "drlancer999",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://scontent.fmel15-2.fna.fbcdn.net/v/t39.30808-6/285495518_7522865197786890_3861121252250812987_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=RAbHbrC_9UkAX8vhaxV&tn=177IfN52zZcuJZLi&_nc_ht=scontent.fmel15-2.fna&oh=00_AfA95aXpz-_TH1DecI1La7z3ajiSObfWVnAWGKsWL_fBOQ&oe=639F8D37",
+    };
+    addDoc(collection(db, "posts"), post);
   }
-  fetchData().catch(console.error)
- }, [])
 
   return (
     <div className="feed">
       <div className="feed__header">
         <h2>Home</h2>
       </div>
-      <TweetBox />
+      <div className="tweetBox">
+        <form action="">
+          <div className="tweetBox__input">
+            <Avatar src={image} />
+            <input
+              value={tweetMessage}
+              className="coloured__input"
+              placeholder="What is Happening?"
+              onChange={(e) => setTweetMessage(e.target.value)}
+            />
+          </div>
+          <input
+            value={tweetImage}
+            className="tweetbox__imageinput"
+            placeholder="Optional: Enter Image URL"
+            type="text"
+            onChange={(e) => setTweetImage(e.target.value)}
+          />
+          <Button onClick={createPost} type="submit" className="tweetBox__btn">
+            Tweet
+          </Button>
+        </form>
+      </div>
+      <FlipMove>
       {posts.map((post) => (
         <Post
           displayName={post.displayName}
@@ -35,6 +88,7 @@ export default function Feed() {
           id={post.id}
         />
       ))}
+      </FlipMove>
     </div>
   );
 }
--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,51 +1,59 @@
-import { Avatar } from '@mui/material'
-import React from 'react'
-import './css/Post.css'
+import { Avatar } from "@mui/material";
+import React, { forwardRef } from "react";
+import "./css/Post.css";
 
-import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import RepeatIcon from '@mui/icons-material/Repeat';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import PublishIcon from '@mui/icons-material/Publish';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import { deleteDoc, doc } from 'firebase/firestore';
-import { db } from './firebase';
+import VerifiedUserIcon from "@mui/icons-material/VerifiedUser";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import PublishIcon from "@mui/icons-material/Publish";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "./firebase";
 
-
-export default function Post({ displayName, username, verified, text, image, avatar, id }) {
+const Post = forwardRef(({ 
+    displayName, username, verified, text, image, avatar, id 
+}, ref) => {
     function deletePost() {
-        const postRef = doc(db, "posts", id)
-        deleteDoc(postRef)
+      const postRef = doc(db, "posts", id);
+      deleteDoc(postRef);
     }
 
-  return (
-    <div className='post'>
+    return (
+      <div className="post" ref={ref}>
         <div className="post__avatar">
-            <Avatar src={avatar} />
+          <Avatar src={avatar} />
         </div>
         <div className="post__body">
-            <div className="post__header">
-                <div className="post__headerText">
-                    <h3 className='username'>
-                        {displayName} <span className='post__headerSpecial'>
-                            {verified && <VerifiedUserIcon className='post__badge' />}
-                            @{username}
-                        </span>
-                    </h3>
-                    <DeleteForeverIcon className='delete__icon' onClick={deletePost} />
-                </div>
-                <div className="post__headerDescription">
-                    <p>{text}</p>
-                </div>
+          <div className="post__header">
+            <div className="post__headerText">
+              <h3 className="username">
+                {displayName}{" "}
+                <span className="post__headerSpecial">
+                  {verified && <VerifiedUserIcon className="post__badge" />}@
+                  {username}
+                </span>
+              </h3>
+              <DeleteForeverIcon
+                className="delete__icon"
+                onClick={deletePost}
+              />
             </div>
-            <img src={image} alt="" />
-            <div className="post__footer">
-                <ChatBubbleOutlineIcon fontSize='small'/>
-                <RepeatIcon fontSize='small'/>
-                <FavoriteBorderIcon fontSize='small'/>
-                <PublishIcon fontSize='small'/>
+            <div className="post__headerDescription">
+              <p>{text}</p>
             </div>
+          </div>
+          <img src={image} alt="" />
+          <div className="post__footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
+          </div>
         </div>
-    </div>
-  )
-}
+      </div>
+    );
+  }
+);
+
+export default Post;
